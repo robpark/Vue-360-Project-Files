15a13de58ca2daff3851523c16264c79abdff443
robpark <robert.d.park@gmail.com>
2020-06-03 22:32:57 -0400

    basic store front with images (needs styling)

--- week2/Project/store-front/woofsrus/src/components/StoreFront.vue
+++ week2/Project/store-front/woofsrus/src/components/StoreFront.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <ul v-for="product in products" :key="product">
-      <li>{{ product }}</li>
-    </ul>
+    <div v-for="product in products" :key="product.id">
+      {{ product.title }}
+      <img :src="require('../assets/' + product.image)" />
+      {{ product.price }}
+    </div>
   </div>
 </template>
 
@@ -13,8 +15,30 @@ export default {
     return {
       product: "",
       products: [
-        'product1',
-        'product2'
+        {
+          id: 1,
+          title: "Bully Slice",
+          price: 998,
+          image: "bully-slice.jpg",
+          availableInventory: 10,
+          rating: 1
+        },
+        {
+          id: 2,
+          title: "Meaty Bone",
+          price: 798,
+          image: "meaty-bone.jpg",
+          availableInventory: 5,
+          rating: 2
+        },
+        {
+          id: 3,
+          title: "Water Friendly Dolphin",
+          price: 1498,
+          image: "water-friendly-dolphin.jpg",
+          availableInventory: 3,
+          rating: 3
+        }
       ]
     };
   }
